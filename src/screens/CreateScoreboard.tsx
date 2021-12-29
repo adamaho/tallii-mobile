969b599875b46e9f9088c109e728258e6afbfa5b
Adam Aho <adam.aho@hey.com>
2021-12-29 18:59:37 -0500

    adding some more stuff

--- src/screens/CreateScoreboard.tsx
+++ src/screens/CreateScoreboard.tsx
@@ -1,25 +1,65 @@
-import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 
-import {View, Button} from 'react-native';
+import * as yup from 'yup';
 
-import {Text} from '../design-system';
+import {SafeAreaView} from 'react-native';
+
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
+import {RootStackParamList} from '../types/screens';
+
+import {useForm} from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers/yup';
+
+import {Row, Box, Column, IconButton, Text} from '../design-system';
+import {DismissKeyboard, Header, TextInputField} from '../components';
+
+const scoreboardSchema = yup.object().shape({
+  name: yup.string().required('need a name for the scoreboard bud'),
+  game: yup.string().required('you forgot to say what game it is'),
+});
 
 export const CreateScoreboard: React.FunctionComponent = () => {
-  const {navigate} = useNavigation();
+  // init navigation
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
-  // React.useEffect(() => {
-  //   console.log('mounted');
-
-  //   return () => {
-  //     console.log('unmounted');
-  //   };
-  // }, []);
+  // init the form
+  const {control, handleSubmit} = useForm({
+    resolver: yupResolver(scoreboardSchema),
+  });
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{fontSize: 30}}>Create Scoreboard</Text>
-      <Button title="Create Team" onPress={() => navigate('CreateTeam')} />
-    </View>
+    <DismissKeyboard>
+      <SafeAreaView style={{flex: 1}}>
+        <Header.Root horizontalAlign="between">
+          <Box />
+          <Header.Title>create scoreboard</Header.Title>
+          {/* TODO: fix this */}
+          <Header.Exit />
+        </Header.Root>
+        <Column padding="default">
+          <TextInputField name="name" label="name" control={control} />
+          <TextInputField name="game" label="game" control={control} />
+          <Row horizontalAlign="between">
+            <Text styledAs="label">teams</Text>
+            <IconButton onPress={() => navigation.navigate('CreateTeam')}>
+              <Text>a</Text>
+            </IconButton>
+          </Row>
+          <Column
+            gap="small"
+            style={{height: 100}}
+            horizontalAlign="center"
+            verticalAlign="center"
+            backgroundColor="widgetSecondary"
+            borderRadius="default"
+          >
+            <Text>❤️</Text>
+            <Text>create team</Text>
+          </Column>
+        </Column>
+      </SafeAreaView>
+    </DismissKeyboard>
   );
 };
