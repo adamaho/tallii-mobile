e97122823e6ccb7e3243e532f29d9b6723ca5a9f
Adam Aho <adam.aho@hey.com>
2022-01-24 16:17:24 -0500

    adding some more stuff

--- src/components/ModalThumb.tsx
+++ src/components/ModalThumb.tsx
@@ -5,7 +5,7 @@ import {Row, Box} from '../design-system';
 export const ModalThumb: React.FunctionComponent = () => {
   return (
     <Row horizontalAlign="center" paddingTop="small">
-      <Box style={{height: 4, width: 60}} borderRadius="round" backgroundColor="widgetTertiary" />
+      <Box style={{height: 5, width: 40}} borderRadius="round" backgroundColor="widgetTertiary" />
     </Row>
   );
 };
